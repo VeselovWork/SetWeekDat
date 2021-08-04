a56eb59226498d97cf3038f85316f779f2c28284
AW <veselovwork@gmail.com>
2021-08-04 16:59:37 +0300

    [main] - 2: final - were is button?

--- SetWeekday/ViewController.swift
+++ SetWeekday/ViewController.swift
@@ -19,7 +19,29 @@ class ViewController: UIViewController {
     
     
     @IBAction func findDay(_ sender: UIButton) {
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(mounthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calendar.date(from: dateComponents)
+        let weekday = dateFormatter.string(from: date!)
+        
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabl.text = capitalizedWeekday
+        
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
 
